ed561d5c2ac2e6e094e6ed7d1c3ae023fcfa802c
faelpatrick <faelpatrick@gmail.com>
2023-03-27 19:23:11 +0100

    End for hour

--- main.js
+++ main.js
@@ -1,19 +1,71 @@
-const robotron = document.querySelector("robotron");
+const maxValue = 10;
+let controles = document.querySelectorAll("[data-controle]");
+let estatisticas = document.querySelectorAll("[data-estatistica]");
 
-let pecas = document.querySelectorAll(".controle-ajuste");
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5,
+  },
 
-pecas.length;
-
-pecas.forEach((element) => {
-  const operacao = element.textContent;
-  const val = element.parentElement.parentElement.querySelector(".controle-contador");
-  element.addEventListener("click", () => manipulaDados(operacao, val));
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20,
+  },
+  nucleos: {
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24,
+  },
+  pernas: {
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42,
+  },
+  foguetes: {
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2,
+  },
+};
+controles.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const operacao = event.target.dataset.controle;
+    const val = event.target.parentNode.querySelector("[data-contador]");
+    const peca = event.target.dataset.peca;
+    manipulaDados(operacao, val, peca);
+  });
 });
 
-function manipulaDados(operacao, val) {
+function manipulaDados(operacao, val, peca) {
   if (operacao === "-") {
-    val.value > 0 ? (val.value = parseInt(val.value) - 1) : (val.value = "00");
+    if (val.value > 0) {
+      val.value = parseInt(val.value) - 1;
+      atualizaEstatisticas(peca, operacao);
+    } else val.value = "00";
   } else {
-    val.value < 99 ? (val.value = parseInt(val.value) + 1) : (val.value = "99");
+    if (val.value < maxValue) {
+      val.value = parseInt(val.value) + 1;
+      atualizaEstatisticas(peca, operacao);
+    } else val.value = maxValue;
   }
+}
+
+function atualizaEstatisticas(peca, operacao) {
+  estatisticas.forEach((element) => {
+    console.log(operacao);
+
+    if (operacao === "-") {
+      element.textContent = parseInt(element.textContent) - pecas[peca][element.dataset.estatistica];
+    } else {
+      element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica];
+    }
+  });
 }
